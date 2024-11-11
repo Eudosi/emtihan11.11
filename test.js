7b02a554318a172d44fcaa434b479f39b99242b9
EldosB <eldosbejsenbaj2@gmail.com>
2024-11-11 10:52:14 +0500

    commit

---
+++ test.js
@@ -0,0 +1,20 @@
+let num1 = prompt("бірінші сан енгізіңіз");
+
+let num2 = prompt("екінші сан енгізіңіз");
+
+let num3 = prompt("үшінші сан енгізіңіз");
+
+alert( num1 > num2  );
+
+
+alert (num1 - num2);
+
+
+
+
+
+
+
+
+
+
